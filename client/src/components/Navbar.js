a33848eefe71a2abf20c9920bd7a7cb3588c6716
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-05-07 14:15:41 +1000

    restarting navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,7 @@
 import React from "react";
 
 function Navbar() {
-  return (
-    <>
-      <section id="navbar">
-        <section className="navbar-links">
-          <ul>
-            <li>Home</li>
-            <li>Categories</li>
-            <li>Subscription</li>
-            <li>Log In</li>
-          </ul>
-        </section>
-      </section>
-    </>
-  );
+  return <></>;
 }
 
 export default Navbar;
