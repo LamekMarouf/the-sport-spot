77a6336446d5c156c213542ce24d643f42ac2a8e
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-05-08 13:49:43 +1000

    added top navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { BiMenuAltLeft, BiCategoryAlt } from "react-icons/bi";
+import { RiHome4Line } from "react-icons/ri";
+import { BsCreditCard2Front } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
 
 function Navbar() {
-  return <></>;
+  return (
+    <>
+      <section className="navbar-top">
+        <div className="logo">The Sport Spot</div>
+        <div className="hamburger">
+          <BiMenuAltLeft />
+        </div>
+      </section>
+
+      <section className="navbar-side">
+        <ul>
+          <li>
+            <RiHome4Line />
+            Home
+          </li>
+          <li>
+            <BiCategoryAlt />
+            Categories
+          </li>
+          <li>
+            <BsCreditCard2Front />
+            Subscription
+          </li>
+          <li>
+            <CgProfile />
+            Log In
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
 
 export default Navbar;
