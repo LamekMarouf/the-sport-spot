2313f3e57a818d20cfc669a189ea3dca6eb55f0b
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-05-09 19:34:22 +1000

    finished premiere league page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles/style.css"
 import Homepage from "./components/Homepage";
 import FootballPage from "./components/FootballPage";
+import PremiereLeaguePage from "./components/PremiereLeaguePage";
 
 function App() {
   return (
     <>
     {/* <Homepage /> */}
-    <FootballPage />
+    {/* <FootballPage /> */}
+    <PremiereLeaguePage />
     </>
   )
 }
---
+++ client/src/components/PremiereLeaguePage.js
@@ -0,0 +1,141 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function PremiereLeaguePage() {
+  return (
+    <>
+      <Navbar />
+      <section className="premiereleague-page">
+        <div className="title">PREMIERE LEAGUE</div>
+        <div className="trending">
+          <div className="trending-title">Trending</div>
+          <div className="trending-cards">
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="all">
+          <div className="all-title">All</div>
+          <div className="all-cards">
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-picture">Insert Picture</div>
+              <div className="card-title">Title</div>
+              <div className="card-time">2 hours ago</div>
+              <div className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                non metus vel risus tempus commodo. Ut felis orci, aliquam at
+                diam sed, scelerisque dapibus ipsum. Sed varius in libero eu
+                blandit. Duis placerat porta massa sit amet pulvinar. Donec
+                blandit tellus ac tincidunt convallis. Proin imperdiet tortor
+                nulla, vel euismod neque ornare at. Cras nisi est, interdum
+                vitae blandit nec, pharetra vel est. Non aliquam. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Quisque non metus
+                vel risus tempus commodo. Ut felis orci, aliquam at diam sed,
+                scelerisque dapibus ipsum. Sed varius in libero eu blandit. Duis
+                placerat porta massa sit amet pulvinar. Donec blandit tellus ac
+                tincidunt convallis.
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default PremiereLeaguePage;
