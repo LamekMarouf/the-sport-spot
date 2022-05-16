83b73e21ae15a913807e1baa3e77c49a12cfabc2
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-05-16 18:07:47 +1000

    fixed comment section

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CommentList = ({ comments = [] }) => {
   if (!comments.length) {
-    return <h3>No Comments Yet</h3>;
+    return <div className="no-comments">No Comments Yet</div>;
   }
 
   return (
